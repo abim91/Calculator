ae3f8955074dadde3edb9a8ff2bb43d9fb3c9487
Abim Shrestha <53064345+abim91@users.noreply.github.com>
2023-08-04 22:33:35 -0400

    add display bar

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var displayValue = "";
+populate();
 function operate(num1,num2,operator){
     if(operator == '+'){
         return (num1 + num2);
@@ -15,10 +17,20 @@ function operate(num1,num2,operator){
 console.log(operate(1,2,'/'));
 
 //creating eventlistener for all buttons
-const blocks = document.getElementsByClassName("key");
-for(let i = 0; i < blocks.length; i++){
-    blocks[i].addEventListener("click", function(){
-        console.log(blocks[i].innerHTML);
-    })
-}
+function populate(){// allow it to enter multiple numbers
+    const displayBox = document.getElementById("display-box");
+    const blocks = document.getElementsByClassName("key");
 
+    for(let i = 0; i < blocks.length; i++){
+        blocks[i].addEventListener("click", function(){
+            if((blocks[i]).classList.contains("operation")){
+                
+            }
+            else{
+                displayValue += blocks[i].innerHTML;
+                displayBox.innerText = displayValue;
+                
+            }
+        })
+    }
+}
